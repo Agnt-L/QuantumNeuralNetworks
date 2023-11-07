c01188bd5792bb8862f05f931f2dc7537e2bdaef
Agnt-L <lenareichard2002@gmail.com>
2023-11-07 13:30:39 +0100

    added docstrings, use adam to adjust learning rate, try batching

--- qnn.py
+++ qnn.py
@@ -6,10 +6,18 @@ import pandas as pd
 import numpy as np
 from qiskit import QuantumCircuit, transpile, execute, Aer, ClassicalRegister, QuantumRegister
 from qiskit.circuit import Parameter
+import random
 
+TRAINABLE_PARAMS = 20
 
 
 def process_data():
+    """
+    Load the Iris dataset, normalize the data, and split it into training and testing sets.
+
+    Returns:
+    x_train, x_test, y_train, y_test: Training and testing data.
+    """
     global data, x_train, y_train
     data = sklearn.datasets.load_iris()
     type(data)
@@ -22,20 +30,22 @@ def process_data():
     X = normalized_df
     y = pd.DataFrame(data.target, columns=["target"])
     x_train, x_test, y_train, y_test = train_test_split(X, y)
-    return (x_train, x_test, y_train, y_test)
+    return x_train, x_test, y_train, y_test
 
 
 def approximate_gradient(parameters, qnn, input_data, target, epsilon=1e-20):
     """
-    Approximate gradient of a loss function at the input predictions.
+    Approximate the gradient of a loss function at the input predictions.
 
     Parameters:
-    - loss_function: A function that takes a predictions as input and returns a scalar loss.
-    - predictions: The predictions at which to compute the gradient.
-    - epsilon: A small value to compute finite differences (default is 1e-6).
+    - parameters: Model parameters to compute the gradient.
+    - qnn: Quantum neural network.
+    - input_data: Input data.
+    - target: Target labels.
+    - epsilon: A small value to compute finite differences (default is 1e-20).
 
     Returns:
-    - gradient: An approximate gradient predictions of the same shape as the input predictions.
+    - gradient: An approximate gradient of the same shape as the input parameters.
     """
     print(parameters)
     gradient = [None] * len(parameters)
@@ -50,8 +60,8 @@ def approximate_gradient(parameters, qnn, input_data, target, epsilon=1e-20):
         predict_plus = predict(parameters_plus, qnn, input_data)
         predict_minus = predict(parameters_minus, qnn, input_data)
 
-        loss_plus = log_loss(y_true=target, y_pred=[predict_plus], labels=[0,1,2])
-        loss_minus = log_loss(y_true=target, y_pred=[predict_minus], labels=[0,1,2])
+        loss_plus = log_loss(y_true=target, y_pred=[predict_plus], labels=[0, 1, 2])
+        loss_minus = log_loss(y_true=target, y_pred=[predict_minus], labels=[0, 1, 2])
 
         # Calculate the finite difference for the i-th component
         gradient[i] = (loss_plus - loss_minus) / (2 * epsilon)
@@ -59,38 +69,130 @@ def approximate_gradient(parameters, qnn, input_data, target, epsilon=1e-20):
     return gradient
 
 
+import random
+import numpy as np
+
+
+def initialize_adam_optimizer(parameters):
+    # Initialize variables for the Adam optimizer
+    m = [0] * len(parameters)  # Initialize first moment estimate
+    v = [0] * len(parameters)  # Initialize second moment estimate
+    beta1 = 0.9  # Decay rate for first moment estimate
+    beta2 = 0.999  # Decay rate for second moment estimate
+    epsilon = 1e-8  # Smoothing term to prevent division by zero
+
+    return m, v, beta1, beta2, epsilon
+
+
+def update_parameters_with_adam(params, loss_gradient, m, v, t, beta1, beta2, epsilon, learning_rate):
+    """
+    Update model parameters using the Adam optimizer.
+
+    Parameters:
+    - params: Model parameters.
+    - loss_gradient: Gradient of the loss.
+    - m: First moment estimate.
+    - v: Second moment estimate.
+    - t: Time step (iteration count).
+    - beta1: Decay rate for the first moment estimate.
+    - beta2: Decay rate for the second moment estimate.
+    - epsilon: Smoothing term to prevent division by zero.
+    - learning_rate: Initial learning rate.
+
+    Returns:
+    updated_params: Updated model parameters.
+    m: Updated first moment estimate.
+    v: Updated second moment estimate.
+    """
+    # Update the first moment estimate
+    m = [(beta1 * m[i] + (1 - beta1) * loss_gradient[i]) for i in range(len(params))]
+
+    # Update the second moment estimate
+    v = [(beta2 * v[i] + (1 - beta2) * (loss_gradient[i] ** 2)) for i in range(len(params))]
+
+    # Bias-corrected first moment estimate
+    m_hat = [m[i] / (1 - beta1 ** t) for i in range(len(params))]
+
+    # Bias-corrected second moment estimate
+    v_hat = [v[i] / (1 - beta2 ** t) for i in range(len(params))]
+
+    # Update the parameters with the adaptive learning rate
+    updated_params = [params[i] - (learning_rate / (np.sqrt(v_hat[i]) + epsilon)) * m_hat[i] for i in
+                      range(len(params))]
+
+    # Enforce the [0, π] constraint
+    updated_params = [max(0, min(np.pi, p)) for p in updated_params]
+
+    # Assign a random value in [0, π] if a parameter reaches 0 or π
+    updated_params = [p if 0 < p < np.pi else random.uniform(0, np.pi) for p in updated_params]
+
+    return updated_params, m, v
+
+
 def build_qc():
-    global qc
+    """
+    Build a Quantum Circuit (qc) and define a parameterized quantum circuit.
+
+    Returns:
+    qc: Quantum Circuit.
+    """
     q = QuantumRegister(11)
     c = ClassicalRegister(3)
     qc = QuantumCircuit(q, c)
+
     # Define the input feature vector
     features = [Parameter(f'f_{i}') for i in range(4)]
-    params = [Parameter(f'p_{i}') for i in range(28)]
+    params = [Parameter(f'p_{i}') for i in range(13)]
+
     for i in range(4):
         qc.rx(features[i], i)
+
     k = 0
-    """# Add parameterized quantum convolution layers
-    for i in range(3):
-        qc.crx(params[k], i, i+1)
-        print(f'k: {k}, i: {i}')
-        k+=1
-    
-    for j in range(4):
-        qc.crx(params[k],i+1,4+j)
-        print(f'k: {k}, i: {i}, j: {j+4}')
-        k+=1
-    
+
     # Add parameterized quantum convolution layers
     for i in range(3):
-        qc.crx(params[k], i + 4, i+5)
+        qc.crx(params[k], i, i + 1)
         print(f'k: {k}, i: {i}')
-        k+=1
-    
+        k += 1
+
+    for j in range(4):
+        qc.crx(params[k], i + 1, 4 + j)
+        print(f'k: {k}, i: {i}, j: {j + 4}')
+        k += 1
+
+    # Add parameterized quantum convolution layers
+    for i in range(3):
+        qc.crx(params[k], i + 4, i + 5)
+        print(f'k: {k}, i: {i}')
+        k += 1
+
     for j in range(3):
-        qc.crx(params[k], i+5, 8+j)
-        print(f'k: {k}, i: {i}, j: {j+5}')
-        k+=1"""
+        qc.crx(params[k], i + 5, 8 + j)
+        print(f'k: {k}, i: {i}, j: {j + 5}')
+        k += 1
+    qc.measure(q[8:], c)
+    return qc
+
+
+def build_qc_big():
+    """
+        Build a Quantum Circuit (qc) and define a parameterized quantum circuit.
+
+        Returns:
+        qc: Quantum Circuit.
+        """
+    q = QuantumRegister(11)
+    c = ClassicalRegister(3)
+    qc = QuantumCircuit(q, c)
+
+    # Define the input feature vector
+    features = [Parameter(f'f_{i}') for i in range(4)]
+    params = [Parameter(f'p_{i}') for i in range(TRAINABLE_PARAMS)]
+
+    for i in range(4):
+        qc.rx(features[i], i)
+
+    k = 0
     for j in range(4):
         # Add parameterized quantum convolution layers
         for i in range(3):
@@ -100,6 +202,7 @@ def build_qc():
         qc.crx(params[k], i + 1, 4 + j)
         print(f'k: {k}, i: {i}, j: {j + 4}')
         k += 1
+
     for j in range(3):
         # Add parameterized quantum convolution layers
         for i in range(3):
@@ -109,26 +212,172 @@ def build_qc():
         qc.crx(params[k], i + 5, 8 + j)
         print(f'k: {k}, i: {i}, j: {j + 5}')
         k += 1
+
     qc.measure(q[8:], c)
-    print(qc.parameters)
     return qc
 
 
-def bind_parameters_to_circuit(qnn, params, values):
-    # Check if the number of parameters matches the number of values
-    if len(params) != len(values):
-        raise ValueError("The number of parameters must match the number of values.")
+def build_qc_ent():
+    """
+        Build a Quantum Circuit (qc) and define a parameterized quantum circuit.
 
-    # Create a parameter binding dictionary
-    parameter_binding = {param: value for param, value in zip(params, values)}
+        Returns:
+        qc: Quantum Circuit.
+        """
+    q = QuantumRegister(11)
+    c = ClassicalRegister(3)
+    qc = QuantumCircuit(q, c)
 
-    # Bind parameters to the circuit
-    bound_circuit = qnn.bind_parameters(parameter_binding)
+    # Define the input feature vector
+    features = [Parameter(f'f_{i}') for i in range(4)]
+    params = [Parameter(f'p_{i}') for i in range(28)]
 
-    return bound_circuit
+    for i in range(4):
+        qc.rx(features[i], i)
+
+    k = 0
+    for j in range(4):
+        # Add parameterized quantum convolution layers
+        for i in range(3):
+            qc.cry(params[k], i, i + 1)
+            qc.cx(i, i + 1)
+            print(f'k: {k}, i: {i}')
+            k += 1
+        qc.crx(params[k], i + 1, 4 + j)
+        print(f'k: {k}, i: {i}, j: {j + 4}')
+        k += 1
+
+    for j in range(3):
+        # Add parameterized quantum convolution layers
+        for i in range(3):
+            qc.cry(params[k], i + 4, i + 5)
+            qc.cx(i + 4, i + 5)
+            print(f'k: {k}, i: {i}')
+            k += 1
+        qc.crx(params[k], i + 5, 8 + j)
+        print(f'k: {k}, i: {i}, j: {j + 5}')
+        k += 1
+
+    qc.measure(q[8:], c)
+    return qc
+
+
+def build_qc_ent_small():
+    """
+        Build a Quantum Circuit (qc) and define a parameterized quantum circuit.
+
+        Returns:
+        qc: Quantum Circuit.
+        """
+    q = QuantumRegister(11)
+    c = ClassicalRegister(3)
+    qc = QuantumCircuit(q, c)
+
+    # Define the input feature vector
+    features = [Parameter(f'f_{i}') for i in range(4)]
+    params = [Parameter(f'p_{i}') for i in range(13)]
+
+    for i in range(4):
+        qc.rx(features[i], i)
+
+    k = 0
+    # Add parameterized quantum convolution layers
+    for i in range(3):
+        qc.cry(params[k], i, i + 1)
+        qc.cx(i, i + 1)
+        print(f'k: {k}, i: {i}')
+        k += 1
+
+    for j in range(4):
+        qc.cry(params[k], i + 1, 4 + j)
+        print(f'k: {k}, i: {i}, j: {j + 4}')
+        k += 1
+
+    # Add parameterized quantum convolution layers
+    for i in range(3):
+        qc.cry(params[k], i + 4, i + 5)
+        qc.cx(i + 4, i + 5)
+        print(f'k: {k}, i: {i}')
+        k += 1
+
+    for j in range(3):
+        qc.cry(params[k], i + 5, 8 + j)
+        print(f'k: {k}, i: {i}, j: {j + 5}')
+        k += 1
+    qc.measure(q[8:], c)
+    return qc
+
+
+def build_qc_ent_small_conv():
+    """
+        Build a Quantum Circuit (qc) and define a parameterized quantum circuit.
+
+        Returns:
+        qc: Quantum Circuit.
+        """
+    q = QuantumRegister(15)
+    c = ClassicalRegister(3)
+    qc = QuantumCircuit(q, c)
+
+    # Define the input feature vector
+    features = [Parameter(f'f_{i}') for i in range(4)]
+    params = [Parameter(f'p_{i}') for i in range(22)]
+
+    for i in range(4):
+        qc.rx(features[i], i)
+
+    k = 0
+    # Add parameterized quantum convolution layers
+    for i in range(3):
+        qc.cry(params[k], i, i + 1)
+        qc.cx(i, i + 1)
+        print(f'k: {k}, i: {i}')
+        k += 1
+
+    for j in range(4):
+        qc.cry(params[k], i + 1, 4 + j)
+        print(f'k: {k}, i: {i}, j: {j + 4}')
+        k += 1
+
+    # Add parameterized quantum convolution layers
+    for i in range(3):
+        qc.cry(params[k], i + 4, i + 5)
+        qc.cx(i + 4, i + 5)
+        print(f'k: {k}, i: {i}')
+        k += 1
+
+    for j in range(4):
+        qc.cry(params[k], i + 5, 8 + j)
+        print(f'k: {k}, i: {i}, j: {j + 4}')
+        k += 1
+
+    # Add parameterized quantum convolution layers
+    for i in range(3):
+        qc.cry(params[k], i + 8, i + 9)
+        qc.cx(i + 8, i + 9)
+        print(f'k: {k}, i: {i}')
+        k += 1
+
+    for j in range(3):
+        qc.cry(params[k], i + 9, 12 + j)
+        print(f'k: {k}, i: {i}, j: {j + 5}')
+        k += 1
+    qc.measure(q[12:], c)
+    return qc
 
 
 def loss_function(qnn, input_data, target_data):
+    """
+    Calculate the loss function using predictions from a quantum neural network.
+
+    Parameters:
+    - qnn: Quantum neural network.
+    - input_data: Input data.
+    - target_data: Target labels.
+
+    Returns:
+    loss: The computed loss.
+    """
     # Perform forward pass to obtain predictions
     predictions = predict(qnn, input_data)
 
@@ -139,12 +388,33 @@ def loss_function(qnn, input_data, target_data):
 
 
 def bind_parameter_values(params, qnn, input_sample):
+    """
+    Bind parameter values to a quantum neural network and input sample.
+
+    Parameters:
+    - params: List of parameter values.
+    - qnn: Quantum neural network.
+    - input_sample: Input data sample.
+
+    Returns:
+    assignedQNN: Quantum neural network with bound parameters.
+    """
     params = input_sample + list(params)
     assignedQNN = qnn.assign_parameters(params)
     return assignedQNN
 
 
 def process_measurement_results(counts, shots):
+    """
+    Process measurement results and calculate qubit probabilities.
+
+    Parameters:
+    - counts: Measurement outcomes.
+    - shots: Number of measurement shots.
+
+    Returns:
+    qubit_probabilities: List of qubit probabilities.
+    """
     # Initialize a list to store the probabilities for each qubit
     qubit_probabilities = [0] * 3
     total_ones = 0
@@ -154,21 +424,35 @@ def process_measurement_results(counts, shots):
         qubit_counts_1 = sum(counts[outcome] for outcome in counts if outcome[qubit] == '1')
         total_ones += qubit_counts_1
         qubit_probabilities[qubit] = qubit_counts_1
-    qubit_probabilities =  [x/total_ones for x in qubit_probabilities]
+    if total_ones > 0:
+        qubit_probabilities = [x / total_ones for x in qubit_probabilities]
+    else:
+        qubit_probabilities = [1 / 3, 1 / 3, 1 / 3]
 
     # Return the list of probabilities
     return qubit_probabilities
 
 
 # Define a prediction function that runs the QNN
-def predict(params, qnn, input_data):
+def predict(params, qnn, input_sample):
+    """
+    Perform quantum circuit execution and process measurement results to make predictions.
+
+    Parameters:
+    - params: Model parameters.
+    - qnn: Quantum neural network.
+    - input_data: Input data.
+
+    Returns:
+    prediction: Predicted values.
+    """
     # Apply the input data to the QNN
     predictions = []
     backend = Aer.get_backend('qasm_simulator')
 
     assignedQNN = bind_parameter_values(params=params, qnn=qnn, input_sample=input_sample)
     transpiled_circuit = transpile(assignedQNN, backend)
-    shots = 1000
+    shots = 100
     job = execute(transpiled_circuit, backend=backend, shots=shots)
     result = job.result()
     counts = result.get_counts()
@@ -179,52 +463,95 @@ def predict(params, qnn, input_data):
 
 # Define a function to update parameters
 def update_parameters(params, loss_gradient, learning_rate):
-    # Iterate through QNN parameters
-    for i in range(len(params)):
-        params[i] = params[i] - learning_rate * loss_gradient[i]
-    return params
+    """
+    Update model parameters using gradient descent.
 
-def train_qnn():
-    global input_sample, target
-    #to be trained
-    params = [1.5] * 28
+    Parameters:
+    - params: Model parameters.
+    - loss_gradient: Gradient of the loss.
+    - learning_rate: Learning rate for gradient descent.
 
-    # Main training loop
-    input_data = x_train.values.tolist()
-    target_data = y_train.values.tolist()
-    num_epochs = 2
-    print(qc.parameters)
-    for epoch in range(1):
-        print("ho")
+    Returns:
+    updated_params: Updated model parameters.
+    """
+    updated_params = [p - learning_rate * g for p, g in zip(params, loss_gradient)]
+
+    # Enforce the [0, π] constraint
+    updated_params = [max(0, min(np.pi, p)) for p in updated_params]
+
+    # Assign a random value in [0, π] if a parameter reaches 0 or π
+    updated_params = [p if 0 < p < np.pi else random.uniform(0, np.pi) for p in updated_params]
+
+    return updated_params
+
+
+def train_qnn(x_train, y_train, batch_size=32):
+    # Build qnn circuit
+    qc = build_qc_ent_small_conv()
+
+    # Initialize model parameters
+    params = [1.5] * TRAINABLE_PARAMS
+    num_epochs = 20
+
+    input_data = x_train.values.tolist()[:50]
+    target_data = y_train.values.tolist()[:50]
+    num_samples = len(input_data)
+
+    # Initialize the Adam optimizer variables
+    t = 0
+    m, v, beta1, beta2, epsilon = initialize_adam_optimizer(
+        params)  # Initialize these variables before the training loop
+
+    for epoch in range(num_epochs):
         total_loss = 0
-        for input_sample, target in zip(input_data, target_data):
-            # Forward pass
-            predictions = predict(params, qc, input_sample)
-            loss = log_loss(y_true=target, y_pred=[predictions], labels=[0,1,2])
-            # predictions = predict(qc, [input_sample])
 
-            # Compute the loss gradient (you need to implement this)
-            loss_gradient = approximate_gradient(params, qc, input_data, target)
-            print(f'gradient: {loss_gradient}')
-            # loss_gradient = compute_loss_gradient(predictions, target)
+        # Split the training data into batches
+        for batch_start in range(0, num_samples, batch_size):
+            batch_end = min(batch_start + batch_size, num_samples)
+            batch_input = input_data[batch_start:batch_end]
+            batch_target = target_data[batch_start:batch_end]
 
-            # Update QNN parameters
-            new_params = update_parameters(params, loss_gradient, 1e-19)
-            params = new_params
+            # Initialize batch gradient
+            batch_gradient = [0.0] * len(params)
 
-            print(f"Params {params}, Loss: {loss}")
+            batch_loss = 0
 
-            # Accumulate the loss
-            # total_loss += loss_function(qnn, [input_sample], [target])
+            for input_sample, target in zip(batch_input, batch_target):
+                predictions = predict(params, qc, input_sample)
+                loss = log_loss(y_true=target, y_pred=[predictions], labels=[0, 1, 2])
+                loss_gradient = approximate_gradient(params, qc, input_sample, target)
 
-        # avg_loss = total_loss / len(input_data)
-        #print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss}")
+                # Accumulate the gradient for this batch
+                batch_gradient = [g + dg for g, dg in zip(batch_gradient, loss_gradient)]
+                total_loss += loss
+                batch_loss += loss
+                print(f'avg batch loss: {batch_loss / batch_size}')
+
+            # Update model parameters with the average gradient for the batch
+            batch_gradient = [g / batch_size for g in batch_gradient]
+            # m, v, beta1, beta2, epsilon = initialize_adam_optimizer(params)
+            # params = update_parameters_with_adam(params, batch_gradient, m, v, beta1, beta2, epsilon, 1e-3, 0.3)
+
+            # Increment the time step (iteration count)
+            t += 1
+
+            # Update parameters using the Adam optimizer
+            params, m, v = update_parameters_with_adam(params, batch_gradient, m, v, t, beta1, beta2, epsilon, 0.3)
+
+            # params = update_parameters(params, batch_gradient, learning_rate=1e-10)
+
+        avg_loss = total_loss / num_samples
+        print(f"Epoch {epoch + 1}/{num_epochs}, Average Loss: {avg_loss}")
 
 
 def main():
+    """
+    Main function to execute the entire script.
+
+    Returns:
+    None
+    """
     x_train, x_test, y_train, y_test = process_data()
-    qc = build_qc()
-    train_qnn()
-    print("hi")
+    train_qnn(x_train, y_train, batch_size=3)
 
 main()
